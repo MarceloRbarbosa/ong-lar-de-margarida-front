ae9803a0445981744ea76826fbcd1d3ba6cad03f
unknown <marceloribeirobarbosa92@gmail.com>
2025-10-16 18:14:38 -0300

    feat: criar função para abrir dropdown navMenu

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+ativarDropdown()
+
+
+function ativarDropdown(){
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("mainNav");
+
+  menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+};
